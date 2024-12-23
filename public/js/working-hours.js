4f2e55a01291cb001a68e55d99ccd3bdebb4e564
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-23 14:21:09 -0300

    feat: create funciona animate progress

---
+++ public/js/working-hours.js
@@ -0,0 +1,28 @@
+function workingHoursEmoji(initialProgress) {
+    return {
+        emoji: '😴',
+        progress: initialProgress,
+      
+        init() {
+            this.$watch('progress', () => this.updateEmoji())
+            this.updateEmoji()
+        },
+        updateEmoji() {
+            let newEmoji;
+            if (this.progress === 0) newEmoji = '😴';
+            else if (this.progress < 25) newEmoji = '🥱';
+            else if (this.progress < 50) newEmoji = '🙂';
+            else if (this.progress < 75) newEmoji = '😅';
+            else if (this.progress <= 100) newEmoji = '😃';
+            else newEmoji = '🎉';
+            
+            if (this.emoji !== newEmoji) {
+                this.$el.style.transform = 'scale(0)';
+                setTimeout(() => {
+                    this.emoji = newEmoji;
+                    this.$el.style.transform = 'scale(1)';
+                }, 300);
+            }
+        }
+    }
+}
