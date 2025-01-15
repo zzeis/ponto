dfaa09ecb4cc38bee36ae1ad7b6630273ca8c8c6
zzeis <moisesmonteiro.stark@gmail.com>
2025-01-15 15:58:21 -0300

    fix: change emoji

--- public/js/working-hours.js
+++ public/js/working-hours.js
@@ -1,28 +1,28 @@
 function workingHoursEmoji(initialProgress) {
     return {
-        emoji: '😴',
+        emoji: "😴",
         progress: initialProgress,
-      
+
         init() {
-            this.$watch('progress', () => this.updateEmoji())
-            this.updateEmoji()
+            this.$watch("progress", () => this.updateEmoji());
+            this.updateEmoji();
         },
         updateEmoji() {
             let newEmoji;
-            if (this.progress === 0) newEmoji = '😴';
-            else if (this.progress < 25) newEmoji = '🥱';
-            else if (this.progress < 50) newEmoji = '🙂';
-            else if (this.progress < 85) newEmoji = '😅';
-            else if (this.progress <= 100) newEmoji = '🥳';
-            else newEmoji = '🎉';
-            
+            if (this.progress === 0) newEmoji = "😴";
+            else if (this.progress < 25) newEmoji = "🥱";
+            else if (this.progress < 50) newEmoji = "🙂";
+            else if (this.progress < 75) newEmoji = "😅";
+            else if (this.progress >= 100) newEmoji = "🥳";
+            else newEmoji = "🎉";
+
             if (this.emoji !== newEmoji) {
-                this.$el.style.transform = 'scale(0)';
+                this.$el.style.transform = "scale(0)";
                 setTimeout(() => {
                     this.emoji = newEmoji;
-                    this.$el.style.transform = 'scale(1)';
+                    this.$el.style.transform = "scale(1)";
                 }, 300);
             }
-        }
-    }
+        },
+    };
 }
